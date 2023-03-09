91812576457989a8362b75c1802c4b01d1671bc0
xuqingxiao <1013966098@qq.com>
2023-03-09 13:41:54 +0800

    feat: 提交

---
+++ src/views/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>1</div>
+  <div>2</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import _ from 'lodash'
+export default defineComponent({
+  setup() {},
+});
+</script>
