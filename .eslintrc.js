2fae983174d3845ee52985972b1bbba51fcd9600
xuqingxiao <1013966098@qq.com>
2023-03-13 17:36:57 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'vue/max-attributes-per-line': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars':'error'
   }
 }
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -8,7 +8,7 @@ import _ from 'loadsh'
 const clickFn = () => {
   const a = 'fd'
   const b = 'aaa'
-  a = '复gf制'
+  a = '测试复gf制'
   console.log(a, 'sdfsd====')
 }
 </script>
